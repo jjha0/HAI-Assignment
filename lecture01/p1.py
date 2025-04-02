25347ff593c6a56cef997bbe405a2ec318047458
김정하 <rilakkuna598@gmail.com>
2025-04-02 23:44:11 +0900

    2주차 과제 제출합니다

--- lecture01/p1.py
+++ lecture01/p1.py
@@ -1,11 +1,11 @@
 class Student:
-    def __init__(self, id, name, age):
-        self.id = id
+    def __init__(self, student_id, name, age):
+        self.student_id = student_id
         self.name = name
         self.age = age
     
     def display_info(self):
-        print("ID: %i번 / 이름: %s / 나이: %i살" %(self.id, self.name, self.age))
+        print("ID: " + self.student_id + " / 이름: " + self.name + " / 나이: " + self.age)
 
 class StudentManager:
     def __init__(self):
@@ -18,9 +18,9 @@ class StudentManager:
         for student in self.students:
             student.display_info()
 
-student1 = Student(1, "김철수", 20)
-student2 = Student(2, "이영희", 21)
-student3 = Student(3, "박지민", 19)
+student1 = Student("1번", "김철수", "20살")
+student2 = Student("2번", "이영희", "21살")
+student3 = Student("3번", "박지민", "19살")
 
 manager = StudentManager()
 manager.add_student(student1)
@@ -30,7 +30,7 @@ manager.add_student(student3)
 print("현재 등록된 학생 목록: ")
 manager.display_all_students()
 
-student4 = Student(4, "한지수", 22)
+student4 = Student("4번", "한지수","22살")
 manager.add_student(student4)
 
 print("\n학번 4번 학생 추가 후: ")
